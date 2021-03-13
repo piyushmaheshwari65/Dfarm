c793929391b0733972ad85d37887a6f3f41c29fd
piyush maheshwari <en17ce301065@medicaps.ac.in>
2021-03-13 14:53:35 +0530

    Adds github pages deployment

---
+++ build/precache-manifest.30bbc59d5e5617c8d2e831ed044865ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f21004b9a9ce8bf12087",
+    "url": "/Dfarm/static/css/main.9ed2031a.chunk.css"
+  },
+  {
+    "revision": "f21004b9a9ce8bf12087",
+    "url": "/Dfarm/static/js/main.f21004b9.chunk.js"
+  },
+  {
+    "revision": "204b98db938162f0a732",
+    "url": "/Dfarm/static/css/1.de0febef.chunk.css"
+  },
+  {
+    "revision": "204b98db938162f0a732",
+    "url": "/Dfarm/static/js/1.204b98db.chunk.js"
+  },
+  {
+    "revision": "c65f06be9edd4c525805",
+    "url": "/Dfarm/static/js/runtime~main.c65f06be.js"
+  },
+  {
+    "revision": "9e19f862110f9f9b844a8fb3473aaac6",
+    "url": "/Dfarm/static/media/photo.9e19f862.png"
+  },
+  {
+    "revision": "0171f5979c049b2e611df5f57c5006b6",
+    "url": "/Dfarm/index.html"
+  }
+];
